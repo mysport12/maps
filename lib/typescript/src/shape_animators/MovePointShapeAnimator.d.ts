e364037ff9ccad62fb0bd2cf365881128bf868cd
Craig Martin <cmart1220@gmail.com>
2024-01-30 11:05:49 -0500

    Include /lib directory

---
+++ lib/typescript/src/shape_animators/MovePointShapeAnimator.d.ts
@@ -0,0 +1,7 @@
+import { Position } from '../types/Position';
+export default class MovePointShapeAnimator {
+    __nativeTag: number;
+    constructor(from: Position);
+    start(): void;
+}
+//# sourceMappingURL=MovePointShapeAnimator.d.ts.map
---
+++ lib/typescript/src/types/Position.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Position is a tuple of latitude and longitude.
+ */
+export declare type Position = [number, number] | number[];
+//# sourceMappingURL=Position.d.ts.map
